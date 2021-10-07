cdf2b401abca2d2a6048c95133b03e0e51ca0e3f
Dragana <dragana.milovancevic@epfl.ch>
2021-10-07 13:56:12 +0200

    [no-ci] Import e2q4

---
+++ src/main/scala/e2q4/E2Q4.scala
@@ -0,0 +1,18 @@
+/* Copyright 2021 EPFL, Lausanne */
+/* Author: Nicolas Matekalo */
+
+package e2q4
+
+import annotation.tailrec
+
+object E2Q4 {
+
+  /**
+    * 
+    *
+    * @param f A function
+    * @return A function that maps an integer into the fixed point of `f`
+    */
+  def fixedPoint(f: Int => Int): Int => Int = ???
+
+}
