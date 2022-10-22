b15d28d5cb5ec56a512d429a848026c970132620
jonas <>
2022-10-22 20:02:27 +0200

    passed all the grading tests

--- src/main/scala/patmat/Huffman.scala
+++ src/main/scala/patmat/Huffman.scala
@@ -343,7 +343,8 @@ trait Huffman extends HuffmanInterface:
   /** This function returns the bit sequence that represents the character
     * `char` in the code table `table`.
     */
-  def codeBits(table: CodeTable)(char: Char): List[Bit] = ???
+  def codeBits(table: CodeTable)(char: Char): List[Bit] =
+    if table.head._1 == char then table.head._2 else codeBits(table.tail)(char)
 
   /** Given a code tree, create a code table which contains, for every character
     * in the code tree, the sequence of bits representing that character.
@@ -353,19 +354,34 @@ trait Huffman extends HuffmanInterface:
     * table. Using the code tables of the sub-trees, think of how to build the
     * code table for the entire tree.
     */
-  def convert(tree: CodeTree): CodeTable = ???
+  def convert(tree: CodeTree): CodeTable =
+    def convertSubTree(tree: CodeTree, acc: List[Bit]): CodeTable =
+      tree match
+        case Leaf(char, weight) => List((char, acc))
+        case Fork(left, right, chars, weight) =>
+          mergeCodeTables(
+            convertSubTree(left, acc :+ 0),
+            convertSubTree(right, acc :+ 1)
+          )
+    convertSubTree(tree, Nil)
 
   /** This function takes two code tables and merges them into one. Depending on
     * how you use it in the `convert` method above, this merge method might also
     * do some transformations on the two parameter code tables.
     */
-  def mergeCodeTables(a: CodeTable, b: CodeTable): CodeTable = ???
+  def mergeCodeTables(a: CodeTable, b: CodeTable): CodeTable =
+    a ::: b
 
   /** This function encodes `text` according to the code tree `tree`.
     *
     * To speed up the encoding process, it first converts the code tree to a
     * code table and then uses it to perform the actual encoding.
     */
-  def quickEncode(tree: CodeTree)(text: List[Char]): List[Bit] = ???
+  def quickEncode(tree: CodeTree)(text: List[Char]): List[Bit] =
+    val bits = codeBits(convert(tree))
+    def encodeText(text: List[Char], acc: List[Bit]): List[Bit] =
+      if text.isEmpty then acc
+      else encodeText(text.tail, acc ::: bits(text.head))
+    encodeText(text, Nil)
 
 object Huffman extends Huffman
